eeb161404c60a8c0dfa94f0c6c890ed8ae925b1d
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-03-31 19:57:26 +0200

    formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss'],
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+	constructor(private test: string, private blub: number) {}
 	title = 'HACAFrontend';
-	test = 'New';
 	array = [{ a: 't', b: { c: 1 } }];
 }
