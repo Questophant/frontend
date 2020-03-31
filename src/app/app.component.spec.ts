d313197a97a11ad6e13d0ed9d415d90377c02ee2
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-03-31 19:58:52 +0200

    formatting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,8 @@ describe('AppComponent', () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		fixture.detectChanges();
 		const compiled = fixture.nativeElement;
-		expect(compiled.querySelector('.content span').textContent).toContain('HACAFrontend app is running!');
+		expect(compiled.querySelector('.content span').textContent).toContain(
+			'HACAFrontend app is running!'
+		);
 	});
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
 	constructor(private test: string, private blub: number) {}
