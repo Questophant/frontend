1696748e66099b0c4d46ab712686a978e0f4651d
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-03-31 17:56:55 +0200

    install and configure prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HACAFrontend';
+	title = 'HACAFrontend';
+	test = 'New';
+	array = [{ a: 't', b: { c: 1 } }];
 }
