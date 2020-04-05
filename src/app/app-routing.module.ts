d95c9330d92ceedf7d49d3628069f4d9de366567
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-04-05 22:16:04 +0200

    create HomePageComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { StartPageComponent } from './pages/start-page/start-page.component';
+import {RegisterPageComponent} from './pages/register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: StartPageComponent,
+	},
+	{
+		path: 'register',
+		component: RegisterPageComponent,
+	},
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import { SharedModule } from './shared/shared/shared.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
+import { SharedModule } from './shared/shared.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 @NgModule({
-	declarations: [
-		AppComponent
-	],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		SharedModule
-	],
+	declarations: [AppComponent, StartPageComponent, RegisterPageComponent],
+	imports: [BrowserModule, AppRoutingModule, SharedModule, FormsModule, ReactiveFormsModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
