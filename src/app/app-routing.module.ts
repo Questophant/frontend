960caf1c8802741e14c98c7969344d75cab17f7b
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-04-08 21:28:42 +0200

    format

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,6 +21,7 @@ const routes: Routes = [
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule],
+	exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
