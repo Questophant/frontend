c1fd6662fb03b33f20ea132ad42fab118ba7e4e6
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-04-08 21:55:54 +0200

    basic style for page

--- src/app/pages/select-challenge-page/select-challenge-page.component.ts
+++ src/app/pages/select-challenge-page/select-challenge-page.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./select-challenge-page.component.scss']
 })
 export class SelectChallengePageComponent implements OnInit {
+
+	challenges: { id: string, name: string; category: string; member: number }[] = [
+		{
+			id: 'challenge-1',
+			name: 'Räum mal auf',
+			category: 'Haushalt',
+			member: 200
+		}
+	];
+
 	constructor() {
 	}
 
