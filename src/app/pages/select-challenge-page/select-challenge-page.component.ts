67c9f93d6e18502a32dbeaf416a5e0a857853876
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-04-11 07:11:10 +0200

    display all categories

--- src/app/pages/select-challenge-page/select-challenge-page.component.ts
+++ src/app/pages/select-challenge-page/select-challenge-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/shared/dtos/category';
 import { ChallengeDto } from 'src/app/shared/dtos/challenge.dto';
 import { ApiService } from 'src/app/shared/services/api-service/api.service';
 
@@ -9,6 +10,7 @@ import { ApiService } from 'src/app/shared/services/api-service/api.service';
 })
 export class SelectChallengePageComponent implements OnInit {
 	challenges$: Promise<ChallengeDto[]>;
+	categories: string[] = Object.keys(Category);
 
 	constructor(private api: ApiService) {
 		this.challenges$ = api.getAllChallenges();
