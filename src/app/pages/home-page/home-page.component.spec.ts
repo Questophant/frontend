674455762bc6482883b4c1b110cb7d9ff6f08e11
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-04-12 23:32:44 +0200

    fix HomePage test setup

--- src/app/pages/home-page/home-page.component.spec.ts
+++ src/app/pages/home-page/home-page.component.spec.ts
@@ -2,14 +2,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ApiService } from 'src/app/shared/services/api-service/api.service';
 import { SimApiService } from 'src/app/shared/services/api-service/sim.api.service';
 import { HomePageComponent } from './home-page.component';
+import { FilterChallengesByCategoryNamePipe } from '../../shared/pipes/filter-challenges-by-category-name-pipe/filter-challenges-by-category-name.pipe';
 
-describe('SelectChallengePageComponent', () => {
+describe('HomePageComponent', () => {
 	let component: HomePageComponent;
 	let fixture: ComponentFixture<HomePageComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [HomePageComponent],
+			declarations: [HomePageComponent, FilterChallengesByCategoryNamePipe],
 			providers: [
 				{
 					provide: ApiService,
