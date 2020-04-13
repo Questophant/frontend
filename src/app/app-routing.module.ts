aa23513ee2287551ac09d65f5701b8bf33805fcc
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-04-13 08:44:59 +0200

    delete RegisterPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
@@ -8,10 +7,6 @@ const routes: Routes = [
 	{
 		path: '',
 		component: WelcomePageComponent,
-	},
-	{
-		path: 'register',
-		component: RegisterPageComponent,
 	},
 	{
 		path: 'select-challenge',
