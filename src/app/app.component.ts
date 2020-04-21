6730a205db632cce54e0f1b98e5de618205ae775
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-04-21 07:35:48 +0200

    add offline message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { ConnectionService } from 'ng-connection-service';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+	offline = false;
+
+	constructor(private connectionService: ConnectionService) {
+		this.connectionService.monitor().subscribe((isConnected) => {
+			this.offline = !isConnected;
+		});
+	}
+}
