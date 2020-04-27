81eef93b541287899ce495cb20630c6a0c4ac901
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-04-27 21:06:12 +0200

    add input fields

--- src/app/pages/create-challenge-page/create-challenge-page.component.ts
+++ src/app/pages/create-challenge-page/create-challenge-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Categories, Category } from '../../shared/dtos/category';
 
 @Component({
 	selector: 'app-create-challenge-page',
@@ -6,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./create-challenge-page.component.scss'],
 })
 export class CreateChallengePageComponent implements OnInit {
+	categories = Categories;
+
+	challengeName = new FormControl('', [Validators.required]);
+	challengeDescription = new FormControl('', [Validators.required]);
+	challengeMaterial = new FormControl('', []);
+	challengeCategory = new FormControl('', [Validators.required]);
+
+	createChallengeForm = new FormGroup({
+		title: this.challengeName,
+		description: this.challengeDescription,
+		material: this.challengeMaterial,
+		category: this.challengeCategory,
+	});
+
 	constructor() {}
 
 	ngOnInit(): void {}
+
+	onSubmit(): void {
+		console.log(this.createChallengeForm);
+	}
 }
