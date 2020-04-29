a83d7754ac5d1d5782dadd391de35940ffe8b400
ALU-42 <andreas@ecogecko.de>
2020-04-30 00:01:49 +0200

    Imprint added, Textpassage added in rules

---
+++ src/app/shared/components/imprint-overlay/imprint-overlay.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ImprintOverlayComponent } from './imprint-overlay.component';
+
+describe('ImprintOverlayComponent', () => {
+  let component: ImprintOverlayComponent;
+  let fixture: ComponentFixture<ImprintOverlayComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ImprintOverlayComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ImprintOverlayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
