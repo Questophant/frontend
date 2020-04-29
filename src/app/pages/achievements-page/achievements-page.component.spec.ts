76a722df49335723f37f27ad2ed2418a17d2df03
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-04-29 07:41:41 +0200

    fix test setup

--- src/app/pages/achievements-page/achievements-page.component.spec.ts
+++ src/app/pages/achievements-page/achievements-page.component.spec.ts
@@ -1,17 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AchievementsPageComponent } from './achievements-page.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ApiService } from '../../shared/services/api-service/api.service';
+import { anyString, instance, mock, when } from 'ts-mockito';
+import { StoreService } from '../../shared/services/store/store.service';
 
 describe('AchievementsPageComponent', () => {
 	let component: AchievementsPageComponent;
 	let fixture: ComponentFixture<AchievementsPageComponent>;
+	const mockApiService = mock(ApiService);
+	const mockStoreService = mock(StoreService);
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
+			providers: [
+				{
+					provide: ApiService,
+					useValue: instance(mockApiService),
+				},
+				{
+					provide: StoreService,
+					useValue: instance(mockStoreService),
+				},
+			],
 			declarations: [AchievementsPageComponent],
 			schemas: [CUSTOM_ELEMENTS_SCHEMA],
 		}).compileComponents();
+
+		when(mockApiService.getAchievementsForUser(anyString())).thenResolve(
+			null
+		);
+		when(mockStoreService.getPublicUserId()).thenReturn('vklkvl');
 	}));
 
 	beforeEach(() => {
