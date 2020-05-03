d627972502ba1edcc96f8c7e1ecc2aa018bb3470
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-05-03 12:51:42 +0200

    add ApiService Provider to test

--- src/app/pages/create-challenge-page/create-challenge-page.component.spec.ts
+++ src/app/pages/create-challenge-page/create-challenge-page.component.spec.ts
@@ -2,14 +2,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateChallengePageComponent } from './create-challenge-page.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ApiService } from '../../shared/services/api-service/api.service';
+import { instance, mock } from 'ts-mockito';
 
 describe('CreateChallengePageComponent', () => {
 	let component: CreateChallengePageComponent;
 	let fixture: ComponentFixture<CreateChallengePageComponent>;
+	const mockApiService = mock(ApiService);
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [CreateChallengePageComponent],
+			providers: [
+				{
+					provide: ApiService,
+					useValue: instance(mockApiService),
+				},
+			],
 			schemas: [CUSTOM_ELEMENTS_SCHEMA],
 		}).compileComponents();
 	}));
