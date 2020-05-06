a0a5a87237cbce42753ee231ba0aec50541d42ef
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-05-06 08:18:48 +0200

    fix lint issues

--- src/app/shared/services/url/url-resolver.service.ts
+++ src/app/shared/services/url/url-resolver.service.ts
@@ -71,7 +71,9 @@ export class UrlResolverService {
 
 		if (!!(elem.getContext && elem.getContext('2d'))) {
 			// was able or not to get WebP representation
-			return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+			return (
+				elem.toDataURL('image/webp').indexOf('data:image/webp') === 0
+			);
 		}
 
 		// very old browser like IE 8, canvas not supported
