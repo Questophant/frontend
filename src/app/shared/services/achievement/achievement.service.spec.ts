d8027ece5be8147b4c8ef580c8fd96ff3ff4a5cf
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-06-07 21:52:39 +0200

    fix test

--- src/app/shared/services/achievement/achievement.service.spec.ts
+++ src/app/shared/services/achievement/achievement.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AchievementService } from './achievement.service';
+import { ApiService } from '../api/api.service';
+import { instance, mock } from 'ts-mockito';
 
 describe('AchievementService', () => {
 	let service: AchievementService;
+	const mockApiService = mock(ApiService);
 
 	beforeEach(() => {
-		TestBed.configureTestingModule({});
+		TestBed.configureTestingModule({
+			providers: [
+				{
+					provide: ApiService,
+					useValue: instance(mockApiService),
+				},
+			],
+		});
 		service = TestBed.inject(AchievementService);
 	});
 
