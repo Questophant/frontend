82e957735bd3553d15c6fcee0b2437b30dde914d
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-06-09 21:26:24 +0200

    update SwUpdate code (#130)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
 import { SwUpdate } from '@angular/service-worker';
 import { ConnectionService } from 'ng-connection-service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
@@ -10,35 +10,28 @@ import { ConnectionService } from 'ng-connection-service';
 })
 export class AppComponent {
 	offline = !navigator.onLine;
-	reloadNeeded = false;
-	nextUpdateCheckDay = new Date().getDate();
 
 	constructor(
 		private connectionService: ConnectionService,
 		swUpdate: SwUpdate,
-		router: Router
+		router: Router,
 	) {
-		this.connectionService.monitor().subscribe((currentState) => {
-			this.offline = !currentState;
-		});
+		this.connectionService.monitor()
+			.subscribe((currentState) => {
+				this.offline = !currentState;
+			});
 
 		// apply updates without reloading the webpage
-		swUpdate.available.subscribe(() => {
-			swUpdate.activateUpdate().then(() => {
-				this.reloadNeeded = true;
+		swUpdate.available
+			.subscribe((e) => {
+				console.log('New version available');
+				swUpdate.activateUpdate()
+					.then(() => {
+						console.log('New version installed. Refreshing ...');
+						document.location.reload();
+					});
 			});
-		});
-		router.events.subscribe((e) => {
-			if (e instanceof NavigationEnd && swUpdate.isEnabled) {
-				const day = new Date().getDate();
-				if (day !== this.nextUpdateCheckDay) {
-					this.nextUpdateCheckDay = day;
-					swUpdate.checkForUpdate();
-				}
-				if (this.reloadNeeded) {
-					window.location.reload();
-				}
-			}
-		});
+
+		router.events.subscribe(swUpdate.checkForUpdate);
 	}
 }
